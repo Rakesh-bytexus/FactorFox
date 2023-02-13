007444168eb2fb791b363c003ac813c6a84dc3ff
rakesh <rakesh@factorfox.com>
2023-02-13 13:44:37 +0530

    dashboard

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LoginPage from './components/login-page'
+import PerformanceModel from './components/performance-model'
+
+const App = () => {
+  return (
+    <div>
+      <PerformanceModel />
+      <LoginPage/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/login-page/login-page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+interface LoginPageProps {
+    email : string
+    password:string
+    isLoggedIn:boolean
+}
+
+const LoginPage = () => {
+  return (
+    <div> 
+
+        <p> hello </p>
+            
+        
+    </div>
+  )
+}
+
+export default LoginPage
